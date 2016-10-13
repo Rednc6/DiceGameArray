fce5ecfd0dbed9666ad8adc177bd70db96bfad51
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-13 13:12:49 +0200

    made die into an array, made it roll said array. More work to come.

--- DiceGame/DiceCup.cs
+++ DiceGame/DiceCup.cs
@@ -8,34 +8,46 @@ namespace DiceGameArray
     class DiceCup
     {
       
-        private Die die1;
-        private Die die2;
-        private Die die3;
+        //private Die die1;
+        //private Die die2;
+        //private Die die3;
+        Die[] numberOfDice;
 
         public DiceCup()
         {
             // VERY IMPORTANT! When we have instance fields of a class type,
             // we MUST remember to create actual objects in the constructor,
             // that the instance fields can then point to.
-            die1 = new Die(10);
-            die2 = new Die(4);
-            die3 = new Die(20);
+            //die1 = new Die(10);
+            //die2 = new Die(4);
+            //die3 = new Die(20);
+            numberOfDice = new Die[4];
+            numberOfDice[0] = new Die(6);
+            numberOfDice[1] = new Die(6);
+            numberOfDice[2] = new Die(10);
+            numberOfDice[3] = new Die(10);
+            numberOfDice[4] = new Die(10);
 
         }
 
-        public DiceCup(int [] terninger )
-        {
+        //public DiceCup(int [] terninger )
+        //{
 
-        }
+        //}
 
         /// <summary>
         /// Ruller alle terningerne i bægeret
         /// </summary>
         public void RollAllDiceInCup()
         {
-            die1.RollDie();
-            die2.RollDie();
-            die3.RollDie();
+            numberOfDice[0].RollDie();
+            numberOfDice[1].RollDie();
+            numberOfDice[2].RollDie();
+            numberOfDice[3].RollDie();
+            numberOfDice[4].RollDie(); 
+            //die1.RollDie();
+            //die2.RollDie();
+            //die3.RollDie();
         }
 
 
@@ -46,7 +58,7 @@ namespace DiceGameArray
         /// <returns></returns>
          public int GetTotalValue()
         {
-            return die1.GetValue() + die2.GetValue() + die3.GetValue();
+            //return die1.GetValue() + die2.GetValue() + die3.GetValue();
         }
 
         // You must create a method with the below header:
